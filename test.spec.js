657c5ba3da3bbc0d2fa1dcd072e200a4b2d492b4
EgeDurukan <ege.durukan1@ibm.com>
2021-10-25 16:18:37 +0200

    second test passes -GREEN

--- test.spec.js
+++ test.spec.js
@@ -2,13 +2,19 @@ const chai = require('chai');
 //import { buildTower } from './buildCastle.js';
 chai.should();
 
-function buildTower () {
-    return false;
+function buildTower (floor) {
+    return validateParameter(floor);
+}
+
+function validateParameter(param) {
+    return typeof param === 'number' && param > 0
 }
 
 describe('test cases for buildCastle function', () => {
     it('The parameter must be number', () => {
         buildTower("One").should.equal(false);
+    })
+    it('The parameter must be bigger than 0', () => {
         buildTower(1).should.equal(true);
     })
 })
